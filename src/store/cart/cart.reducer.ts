330c7c245373c8956c2625c862dd3e987b605240
shubh batra <godwithsam@gmail.com>
2023-05-04 20:19:40 +0100

    Types cart files.

---
+++ src/store/cart/cart.reducer.ts
@@ -0,0 +1,35 @@
+import { AnyAction } from "redux";
+import { CartItem } from "./cart.types";
+import { setCartItems, setIsCartOpen } from "./cart.action";
+
+
+export type CartState ={
+    isCartOpen: boolean;
+    cartItems: CartItem[];
+}
+
+export const CART_INITIAL_STATE : CartState= {
+    isCartOpen:false,
+    cartItems: [],
+};
+
+
+// reducer is used when one state changes many other values using a single approach. this is the reason we used reducer here.
+export const cartReducer = (state = CART_INITIAL_STATE,action: AnyAction ): CartState => {
+
+    if(setIsCartOpen.match(action)){
+        return {
+            ...state,
+            isCartOpen: action.payload,  
+        };
+    }
+
+    if(setCartItems.match(action)){
+        return{
+            ...state,
+            cartItems: action.payload,
+        };
+    }
+
+    return state;
+};
