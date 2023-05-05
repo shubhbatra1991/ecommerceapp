f32438253eeba0d71f9c45c0b5b1288a77ce345b
shubh batra <godwithsam@gmail.com>
2023-05-05 13:48:38 +0100

    edited cart selector file.

--- src/store/cart/cart.selector.ts
+++ src/store/cart/cart.selector.ts
@@ -1,30 +1,29 @@
-import { createSelector } from "reselect";
-import { CartState } from "./cart.reducer";
+import { createSelector } from 'reselect';
+import { CartState } from './cart.reducer';
 
 const selectCartReducer = (state): CartState => state.cart;
 
 export const selectCartItems = createSelector(
-    [selectCartReducer],
-    (cart) => cart.cartItems
+  [selectCartReducer],
+  (cart) => cart.cartItems
 );
 
-export const selectIsCarOpen = createSelector(
-    [selectCartReducer],
-    (cart) => cart.isCartOpen
+export const selectIsCartOpen = createSelector(
+  [selectCartReducer],
+  (cart) => cart.isCartOpen
 );
 
 export const selectCartCount = createSelector(
-    [selectCartItems],
-    (cartItems) => cartItems.reduce(
-        (total, cartItem) => total + cartItem.quantity, 
-        0
-    )
+  [selectCartItems],
+  (cartItems): number =>
+    cartItems.reduce((total, cartItem) => total + cartItem.quantity, 0)
 );
 
-export const selectCartTotal =  createSelector(
-    [selectCartItems],
-    (cartItems) => cartItems.reduce(
-        (total, cartItem) => total + cartItem.quantity * cartItem.price, 
-        0
+export const selectCartTotal = createSelector(
+  [selectCartItems],
+  (cartItems): number =>
+    cartItems.reduce(
+      (total, cartItem) => total + cartItem.quantity * cartItem.price,
+      0
     )
 );
