4215e95f13da6569fd874da4d3c55aedd45ad780
shubh batra <godwithsam@gmail.com>
2023-05-08 00:07:53 +0100

    Configured category ts file.

--- src/store/user/user.saga.ts
+++ src/store/user/user.saga.ts
@@ -23,58 +23,58 @@ import {
 
 export function* getSnapshotFromUserAuth(userAuth, additionalDetails){
     try{
-        const userSnapshot = yield call(createUserDocumentFromAuth, userAuth, additionalDetails);
-        yield put(signInSuccess({ id: userSnapshot.id, ...userSnapshot.data() }));
+        const userSnapshot = yield* call(createUserDocumentFromAuth, userAuth, additionalDetails);
+        yield* put(signInSuccess({ id: userSnapshot.id, ...userSnapshot.data() }));
 
     } catch (error){
-        yield put(signInFailed(error));
+        yield* put(signInFailed(error));
     }
 };
 
 
 export function* signInWithGoogle(){
     try{
-        const { user } = yield call(signInWithGooglePopup);
-        yield call(getSnapshotFromUserAuth, user);
+        const { user } = yield* call(signInWithGooglePopup);
+        yield* call(getSnapshotFromUserAuth, user);
     } catch(error){
-        yield put(signInFailed(error));
+        yield* put(signInFailed(error));
     }
 };
 
 export function* signInWithEmail({ payload: { email, password } }) {
     try {
-      const { user } = yield call(
+      const { user } = yield* call(
         signInAuthUserWithEmailAndPassword,
         email,
         password
       );
-      yield call(getSnapshotFromUserAuth, user);
+      yield* call(getSnapshotFromUserAuth, user);
     } catch (error) {
-      yield put(signInFailed(error));
+      yield* put(signInFailed(error));
     }
   }
 
 export function* isUserAuthenticated(){
     try{
-        const userAuth = yield call(getCurrentUser);
+        const userAuth = yield* call(getCurrentUser);
         if(!userAuth) return;
-        yield call(getSnapshotFromUserAuth, userAuth);
+        yield* call(getSnapshotFromUserAuth, userAuth);
 
     } catch(error){
-        yield put(signInFailed(error));
+        yield* put(signInFailed(error));
     }
 };
 
 export function* signUp({ payload: { email, password, displayName } }) {
     try {
-      const { user } = yield call(
+      const { user } = yield* call(
         createAuthUserWithEmailAndPassword,
         email,
         password
       );
-      yield put(signUpSuccess(user, { displayName }));
+      yield* put(signUpSuccess(user, { displayName }));
     } catch (error) {
-      yield put(signUpFailed(error));
+      yield* put(signUpFailed(error));
     }
   }
 
