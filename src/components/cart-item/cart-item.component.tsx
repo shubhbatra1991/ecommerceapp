f28871974867986c4b694a24175a630f7dcdd880
shubh batra <godwithsam@gmail.com>
2023-05-08 02:35:50 +0100

    Configured the structure as TS files.

---
+++ src/components/cart-item/cart-item.component.tsx
@@ -0,0 +1,23 @@
+
+import { FC } from 'react';
+import { CartItemContainer, ItemDetails } from './cart-item.styles';
+import { CartItem as TCartItem } from '../../store/cart/cart.types';
+
+type CartItemProps ={
+    cartItem: TCartItem;
+}
+
+export const CartItem: FC<CartItemProps>  = ({cartItem}) => {
+    const { name, imageUrl, price, quantity } = cartItem;
+    return(
+        <CartItemContainer>
+      <img src={imageUrl} alt={`${name}`} />
+      <ItemDetails>
+        <span>{name}</span>
+        <span>
+          {quantity} x ${price}
+        </span>
+      </ItemDetails>
+    </CartItemContainer>
+  );
+};
---
+++ src/routes/authentication/authentication.component.tsx
@@ -0,0 +1,14 @@
+import SignUpForm from '../../components/sign-up/sign-up-form.component';
+import SignInForm from '../../components/sign-in-form/sign-in-form.component';
+import { AuthenticationContainer } from './authentication.style';
+  
+  const Authentication = () => { 
+    return (
+      <AuthenticationContainer>
+      <SignInForm />
+      <SignUpForm />
+    </AuthenticationContainer>
+  );
+};
+  
+  export default Authentication;
